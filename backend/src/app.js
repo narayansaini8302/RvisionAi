4a8afb09938009eef3191e9458092f33444ab97d
Narayan Lal Mali <narayansaini8302@gmail.com>
2026-04-08 17:23:08 +0530

    api changed

--- backend/src/app.js
+++ backend/src/app.js
@@ -70,8 +70,8 @@ app.get('/', (req, res) => {
 const authRouter = require('./routes/auth.route');
 const interviewRouter = require('./routes/interview.route');
 
-app.use('/api', authRouter);
-app.use('/api', interviewRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/interview', interviewRouter);
 
 // 404 handler
 app.use((req, res) => {
